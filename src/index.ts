6e23bb15ff87d3d20dc028aaca37ced88d335a2f
Jhon Rey <jhonreymendiola@gmail.com>
2024-01-29 19:23:22 +0800

    deployment fix

--- src/index.ts
+++ src/index.ts
@@ -10,22 +10,41 @@ var url = process.env.URI;
 const local = 3000
 const dev = "https://jac-booking-api.vercel.app/"
 
-export async function start() {
+
+export const app = express();
+app.use(body.json({
+  limit: '500kb'
+}))
+app.use('/customers', require('./routes/customers'));
+
+app.listen(local, async () => {
   try{
-    const app = express();
-    app.use(body.json({
-      limit: '500kb'
-    }))
-    app.use('/customers', require('./routes/customers'));
+    if (!url) throw new Error('Mongo URI unavailable');
+    await mongoose.connect(url);
+  
+    console.log('Server is running on port 3000');
 
-    app.listen(local, async () => {
-      if (!url) throw new Error('Mongo URI unavailable');
-      await mongoose.connect(url);
-
-      console.log('Server is running on port 3000');
-    })
-  }catch(error){
-    console.log(error);
+  }catch (ex) {
+    console.error(ex || "");
   }
-}
-start();
+})
+
+// export async function start() {
+//   try{
+//     const app = express();
+//     app.use(body.json({
+//       limit: '500kb'
+//     }))
+//     app.use('/customers', require('./routes/customers'));
+
+//     app.listen(local, async () => {
+//       if (!url) throw new Error('Mongo URI unavailable');
+//       await mongoose.connect(url);
+
+//       console.log('Server is running on port 3000');
+//     })
+//   }catch(error){
+//     console.log(error);
+//   }
+// }
+// start();
