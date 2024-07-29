4ac0ba990a802a89c41b83d168526b112d387591
L99-jmendiola <jhonreymendiola@gmail.com>
2024-07-29 09:37:54 +0800

    verify email api route

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -54,6 +54,24 @@ export const getAllUsers = async (req: Request, res: Response) => {
   }
 };
 
+const verifyEmail = async (req: Request, res: Response) => {
+  const { token } = req.body;
+  try {
+    const user = await User.findOne({ verificationToken: token });
+    console.log('j',token,user)
+    if (!user) {
+      return res.status(400).json({ error: 'Invalid or expired token' });
+    }
+    user.isVerified = true;
+    user.verificationToken = undefined;
+    await user.save();
+    res.status(200).json({ message: 'Email verified successfully!' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
 router.post('/', registerController)
 router.post('/login', loginController)
 router.post('/forgotpassword', forgotPasswordController)
@@ -61,5 +79,6 @@ router.post('/resetPassword', resetPasswordController)
 router.get('/messages',authenticateToken, getUserPrompts)
 router.delete('/messages/:messageId', authenticateToken, deleteUserPrompt);
 router.get('/users', authenticateToken, authorizeAdmin, getAllUsers);
+router.post('/verifyEmail', verifyEmail)
 
 module.exports = router;
